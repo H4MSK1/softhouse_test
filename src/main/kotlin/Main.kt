e18002901bf1cc171a858cdaabecb998657ab515
Mohammad Alburhan <alburhan97@gmail.com>
2020-05-01 03:58:17 +0200

    Refactor main method to accept second argument as the output file dest

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,15 @@
-const val DEFAULT_FILE_SOURCE = "person_data.txt";
-const val OUTPUT_FILE_SOURCE = "output.xml";
+import java.io.IOException
+
+const val DEFAULT_FILE_SOURCE = "person_data.txt"
+const val OUTPUT_FILE_SOURCE = "output.xml"
 
 fun main(args: Array<String>) {
-    val fileName = args.getOrElse(0){ DEFAULT_FILE_SOURCE}
-    PersonDataParser().parse(fileName, OUTPUT_FILE_SOURCE)
+    val source = args.getOrElse(0) { DEFAULT_FILE_SOURCE }
+    val destination = args.getOrElse(1) { OUTPUT_FILE_SOURCE }
+
+    try {
+        PersonDataParser().parse(source, destination)
+    } catch (err: IOException) {
+        err.printStackTrace()
+    }
 }
