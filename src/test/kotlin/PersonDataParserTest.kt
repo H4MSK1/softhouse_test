e31209ebd14d62bc03647f8e468339471baa56db
Mohammad Alburhan <alburhan97@gmail.com>
2020-05-01 03:59:32 +0200

    Added testcases for PersonDataParser class

---
+++ src/test/kotlin/PersonDataParserTest.kt
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import java.io.File
+import java.io.IOException
+
+
+class PersonDataParserTest {
+
+    @Test
+    fun parseNonExistingSourceFile() {
+        assertThrows<IOException> {
+            PersonDataParser().parse("i dont exist", "")
+        }
+    }
+
+    @Test
+    fun parseFromSourceFile() {
+        val classLoader = javaClass.classLoader
+        val sourceFile = File(classLoader.getResource("person_data.txt").file).path
+        val outputDestination = File(classLoader.getResource("output.xml").file).path
+
+        assertDoesNotThrow {
+            val parser = PersonDataParser()
+            parser.parse(sourceFile, outputDestination)
+        }
+    }
+}
