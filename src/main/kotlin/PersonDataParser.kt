1092c5a77e10858bf30ba646021ad3160da321aa
Mohammad Alburhan <alburhan97@gmail.com>
2020-05-01 23:43:10 +0200

    Added chunk processing and batch write

--- src/main/kotlin/PersonDataParser.kt
+++ src/main/kotlin/PersonDataParser.kt
@@ -1,32 +1,84 @@
+import com.sun.xml.internal.txw2.output.IndentingXMLStreamWriter
 import org.apache.commons.io.FileUtils
 import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
+import javax.xml.stream.XMLOutputFactory
+import javax.xml.stream.XMLStreamWriter
 
 class PersonDataParser {
 
-    @Throws(IOException::class)
-    fun parse(fileName: String, outputPath: String) {
+    fun parse(fileName: String, outputPath: String) = measureExecutionTime({ timeElapsed ->
+        println("Total execution time: $timeElapsed milliseconds")
+    }) {
+        println("Reading person data from source: $fileName")
         readPersonDataFromFile(fileName, outputPath)
         println("Person data has successfully been migrated to output file: $outputPath")
     }
 
+    private fun processChunk(
+        writer: XMLStreamWriter,
+        xmlDocBuilder: XMLDocumentBuilder,
+        personCollection: PersonCollection,
+        chunkCounter: Int,
+        personCounter: Int
+    ) {
+        println("Processing chunk number ($chunkCounter) with ($personCounter) person nodes" + System.lineSeparator())
+        xmlDocBuilder.transformPersonNodeToXML(writer, personCollection.convertNodesToPersonObjects())
+        writer.flush()
+    }
+
     @Throws(IOException::class)
     private fun readPersonDataFromFile(path: String, outputPath: String) {
-        FileUtils.lineIterator(File(path), "UTF-8").use { it ->
-            val personCollection = PersonCollection()
+        FileUtils.lineIterator(File(path), "UTF-8").use { iterator ->
+            val xmlDocBuilder = XMLDocumentBuilder()
+            val writer: XMLStreamWriter = IndentingXMLStreamWriter(
+                XMLOutputFactory
+                    .newInstance()
+                    .createXMLStreamWriter(FileOutputStream(outputPath), "UTF-8")
+            )
+            writer.writeStartDocument("UTF-8", "1.0")
+            writer.writeStartElement("people")
 
-            while (it.hasNext()) {
-                val line: String = it.nextLine()
+            try {
+                val personCollection = PersonCollection()
+                val chunkSize = 100
 
-                when (line[0]) {
-                    'P' -> personCollection.push(line, PersonCollection.ElementType.P)
-                    'T' -> personCollection.push(line, PersonCollection.ElementType.T)
-                    'A' -> personCollection.push(line, PersonCollection.ElementType.A)
-                    'F' -> personCollection.push(line, PersonCollection.ElementType.F)
+                var chunkCounter = 1
+                var personNodeCounter = 0
+                var shouldProcessChunk = false
+
+                while (iterator.hasNext()) {
+                    val line: String = iterator.nextLine()
+                    when (line[0]) {
+                        'P' -> {
+                            if (shouldProcessChunk) {
+                                processChunk(writer, xmlDocBuilder, personCollection, chunkCounter, personNodeCounter)
+                                chunkCounter++
+                                personNodeCounter = 1
+                            } else {
+                                personNodeCounter++
+                            }
+
+                            personCollection.push(line, PersonCollection.ElementType.P)
+                        }
+                        'T' -> personCollection.push(line, PersonCollection.ElementType.T)
+                        'A' -> personCollection.push(line, PersonCollection.ElementType.A)
+                        'F' -> personCollection.push(line, PersonCollection.ElementType.F)
+                    }
+
+                    shouldProcessChunk = personNodeCounter == chunkSize
                 }
-            }
 
-            XMLDocumentBuilder().savePersonsAsXML(outputPath, personCollection.convertNodesToPersonObjects())
+                if (personNodeCounter > 0) {
+                    processChunk(writer, xmlDocBuilder, personCollection, chunkCounter, personNodeCounter)
+                }
+            } catch (err: Exception) {
+                err.printStackTrace()
+            } finally {
+                writer.writeEndElement()
+                writer.close()
+            }
         }
     }
 }
