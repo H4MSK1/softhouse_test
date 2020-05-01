6abbd019529b596e331e105dc217b1b5347f786f
Mohammad Alburhan <alburhan97@gmail.com>
2020-05-01 23:44:10 +0200

    Refactored to use XMLStreamWriter to write to buffer with chunks process

--- src/main/kotlin/XMLDocumentBuilder.kt
+++ src/main/kotlin/XMLDocumentBuilder.kt
@@ -1,93 +1,70 @@
 import Person.*
-import org.w3c.dom.Document
-import org.w3c.dom.Element
-import java.io.File
-import javax.xml.parsers.DocumentBuilderFactory
-import javax.xml.parsers.ParserConfigurationException
-import javax.xml.transform.Transformer
-import javax.xml.transform.TransformerException
-import javax.xml.transform.TransformerFactory
-import javax.xml.transform.dom.DOMSource
-import javax.xml.transform.stream.StreamResult
+import javax.xml.stream.XMLStreamWriter
 
 class XMLDocumentBuilder {
-    private fun buildPhoneXMLNode(document: Document, phoneNumber: PhoneNumber): Element {
-        val root = document.createElement("phone")
-        root.appendChild(buildXMLNodeFromValue(document, "mobile", phoneNumber.mobile))
-        root.appendChild(buildXMLNodeFromValue(document, "landline", phoneNumber.landline))
-        return root
+
+    private fun writeNodeToBuffer(writer: XMLStreamWriter, attribute: String, value: String) {
+        writer.writeStartElement(attribute)
+        writer.writeCharacters(value)
+        writer.writeEndElement()
     }
 
-    private fun buildAdressXMLNode(document: Document, adress: Adress): Element {
-        val root = document.createElement("adress")
-        root.appendChild(buildXMLNodeFromValue(document, "street", adress.street))
-        root.appendChild(buildXMLNodeFromValue(document, "city", adress.city))
-        if (adress.zipcode.isNotEmpty()) {
-            root.appendChild(buildXMLNodeFromValue(document, "zipcode", adress.zipcode))
-        }
-        return root
-    }
+    private fun writePersonFamilyToBuffer(writer: XMLStreamWriter, family: List<Family>) {
+        for (familyMember in family) {
+            writer.writeStartElement("family")
+            writeNodeToBuffer(writer, "name", familyMember.name)
+            writeNodeToBuffer(writer, "born", familyMember.born)
 
-    private fun buildFamilyXMLNode(document: Document, root: Element, familyList: List<Family>) {
-        for (familyMember in familyList) {
-            val familyRoot = document.createElement("family")
-            familyRoot.appendChild(buildXMLNodeFromValue(document, "name", familyMember.name))
-            familyRoot.appendChild(buildXMLNodeFromValue(document, "born", familyMember.born))
-
-            when {
-                familyMember.adress != null ->
-                    familyRoot.appendChild(buildAdressXMLNode(document, familyMember.adress!!))
-                familyMember.phoneNumber != null ->
-                    familyRoot.appendChild(buildPhoneXMLNode(document, familyMember.phoneNumber!!))
+            if (familyMember.adress != null) {
+                writePersonAddressToBuffer(writer, familyMember.adress!!)
             }
 
-            root.appendChild(familyRoot)
+            if (familyMember.phoneNumber != null) {
+                writePersonPhoneNumberToBuffer(writer, familyMember.phoneNumber!!)
+            }
+
+            writer.writeEndElement()
         }
     }
 
-    private fun buildXMLNodeFromValue(document: Document, attribute: String, attributeValue: String): Element {
-        val attributeNode = document.createElement(attribute)
-        attributeNode.appendChild(document.createTextNode(attributeValue))
-        return attributeNode
+    private fun writePersonAddressToBuffer(writer: XMLStreamWriter, adress: Adress) {
+        writer.writeStartElement("address")
+        writeNodeToBuffer(writer, "street", adress.street)
+        writeNodeToBuffer(writer, "city", adress.city)
+        writeNodeToBuffer(writer, "zipcode", adress.zipcode)
+        writer.writeEndElement()
     }
 
-    fun savePersonsAsXML(xmlOutputFilePath: String, personCollection: List<Person>) = try {
-        val document = DocumentBuilderFactory.newInstance()
-            .newDocumentBuilder()
-            .newDocument()
+    private fun writePersonPhoneNumberToBuffer(writer: XMLStreamWriter, phoneNumber: PhoneNumber) {
+        writer.writeStartElement("phone")
+        writeNodeToBuffer(writer, "mobile", phoneNumber.mobile)
+        writeNodeToBuffer(writer, "landline", phoneNumber.landline)
+        writer.writeEndElement()
+    }
 
-        val root = document.createElement("people")
-        document.appendChild(root)
+    fun transformPersonNodeToXML(writer: XMLStreamWriter, personCollection: List<Person>) {
+        val iterator = personCollection.listIterator() as MutableListIterator<Person>
+        while (iterator.hasNext()) {
+            val person = iterator.next()
 
-        for (personObj in personCollection) {
-            val personRoot = document.createElement("person")
-            personRoot.appendChild(buildXMLNodeFromValue(document, "firstname", personObj.firstname))
-            personRoot.appendChild(buildXMLNodeFromValue(document, "lastname", personObj.lastname))
+            writer.writeStartElement("person")
+            writeNodeToBuffer(writer, "firstname", person.firstname)
+            writeNodeToBuffer(writer, "lastname", person.lastname)
 
-            if (personObj.personAdress != null) {
-                personRoot.appendChild(buildAdressXMLNode(document, personObj.personAdress!!))
-            }
-            if (personObj.personPhoneNumber != null) {
-                personRoot.appendChild(buildPhoneXMLNode(document, personObj.personPhoneNumber!!))
-            }
-            if (personObj.hasFamily()) {
-                buildFamilyXMLNode(document, personRoot, personObj.familyList)
+            if (person.personAdress != null) {
+                writePersonAddressToBuffer(writer, person.personAdress!!)
             }
 
-            root.appendChild(personRoot)
+            if (person.personPhoneNumber != null) {
+                writePersonPhoneNumberToBuffer(writer, person.personPhoneNumber!!)
+            }
+
+            if (person.hasFamily()) {
+                writePersonFamilyToBuffer(writer, person.familyList)
+            }
+
+            writer.writeEndElement()
+            iterator.remove()
         }
-
-        val transformer: Transformer = TransformerFactory
-            .newInstance()
-            .newTransformer()
-
-        transformer.transform(
-            DOMSource(document),
-            StreamResult(File(xmlOutputFilePath))
-        )
-    } catch (err: ParserConfigurationException) {
-        err.printStackTrace()
-    } catch (err: TransformerException) {
-        err.printStackTrace()
     }
 }
